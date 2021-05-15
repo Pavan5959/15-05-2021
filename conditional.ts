9cb2abec07d89171f1b933fea20612e4e4a7eb2f
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-05-15 23:55:02 +0530

    Create conditional.ts

---
+++ conditional.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'Angular-APP';
+   show = true;
+   shows = "yes"; //with String
+}
