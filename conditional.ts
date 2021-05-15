6d6154ff790e1268f6844b5af45b5cabf022a142
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-05-16 00:04:09 +0530

    Update conditional.ts

--- conditional.ts
+++ conditional.ts
@@ -11,3 +11,17 @@ export class AppComponent
    show = true;
    shows = "yes"; //with String
 }
+
+//another If Block
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'Angular-APP';
+   show = "red";
+}
